b31cb794ddf2265d353e3553f4c2820c4920bd89
dean <dean@dbassett.dev>
2022-01-02 16:46:43 -0800

    nicer

--- mod.ts
+++ mod.ts
@@ -3,8 +3,13 @@ import {
 } from 'https://deno.land/x/harmony@v2.4.0/mod.ts';
 import { serve, PathParams } from "https://deno.land/x/sift@0.4.2/mod.ts";
 
+const TOKEN = Deno.env.get('DISCORD_BOT_TOKEN')!;
+const CHANNEL = Deno.env.get('DISCORD_BOT_CHANNEL')!;
+
+
 const clientInfo = new Client({
-  token: Deno.env.get('DISCORD_BOT_TOKEN')!,
+  token: TOKEN,
+  intents: [],
 });
 
 const client = await clientInfo.connect();
@@ -13,7 +18,7 @@ serve({
   "/": main,
 });
 
-function main(request: Request, _params: PathParams): Response {
-  client.channels.sendMessage(Deno.env.get('DISCORD_BOT_CHANNEL')!, 'lmao');
+function main(_request: Request, _params: PathParams): Response {
+  client.channels.sendMessage(CHANNEL, '927352929239834714');
   return new Response('', { status: 200 });
 }
